52fcf73be050cd099501adf2db2c6e7bc7ff9f7e
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-12-07 04:01:07 +0100

    show price

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,28 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+
+const get = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+const price = ref();
+
+const fetchPrice = () => {
+  get('https://blockchain.info/tobtc?currency=USD&value=1').then(res => {
+    price.value = (1 / res).toFixed(2);
+    console.log(price.value);
+  }).finally(err => {
+    setTimeout(fetchPrice, 15e3);
+  });
+};
+
+fetchPrice();
+
 </script>
 
 <template>
-  hello
+  1 BTC = {{ price }} USD
 </template>
 
 <style scoped>
